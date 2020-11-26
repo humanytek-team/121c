d5194bf5f49e76f1f0393d34d5461e5433c9a19b
Moisés Navarro <moisalejandro@gmail.com>
2020-11-26 12:45:27 -0600

    [MIG] rem api.multi

--- models/project_task.py
+++ models/project_task.py
@@ -2,12 +2,11 @@ from odoo import api, fields, models, _
 
 
 class ProjectTask(models.Model):
-    _inherit = 'project.task'
+    _inherit = "project.task"
 
-    date_deadline = fields.Datetime(
-    )
+    date_deadline = fields.Datetime()
     remaining_hours_to_deadline = fields.Float(
-        compute='_compute_remaining_hours_to_deadline',
+        compute="_compute_remaining_hours_to_deadline",
         # store=True,
         # readonly=True,
     )
@@ -15,25 +14,26 @@ class ProjectTask(models.Model):
     def _compute_remaining_hours_to_deadline(self):
         for task in self:
             if task.date_deadline:
-                task.remaining_hours_to_deadline = (task.date_deadline - fields.Datetime.now()).total_seconds() / 60 / 60 # secods / minutes / hours
-    
-    @api.onchange('stage_id')
+                task.remaining_hours_to_deadline = (
+                    (task.date_deadline - fields.Datetime.now()).total_seconds() / 60 / 60
+                )  # secods / minutes / hours
+
+    @api.onchange("stage_id")
     def _on_change_stage(self):
-        if self._origin.stage_id.name == 'WIP' and self.stage_id.name == 'Review':
+        if self._origin.stage_id.name == "WIP" and self.stage_id.name == "Review":
             return {
-                'warning': {
-                    'title': _('Stage advanced'),
-                    'message': _('Did you register your hours?'),
+                "warning": {
+                    "title": _("Stage advanced"),
+                    "message": _("Did you register your hours?"),
                 }
             }
-    
+
     @api.model
     def create(self, vals):
         task = super(ProjectTask, self).create(vals)
         task.sudo().message_follower_ids.unlink()
         return task
 
-    @api.multi
     def message_subscribe(self, partner_ids=None, channel_ids=None, subtype_ids=None):
         res = super(ProjectTask, self).message_subscribe(partner_ids, channel_ids, subtype_ids)
         self.project_id.message_subscribe(partner_ids=self.message_partner_ids.ids, subtype_ids=[1])
