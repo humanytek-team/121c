c7595cd81c34bd4bb5fc85d50cee54a0a47e352f
Moisés Navarro <moisalejandro@gmail.com>
2020-12-14 14:03:05 -0600

    [FIX]

--- models/account_move.py
+++ models/account_move.py
@@ -10,7 +10,7 @@ class AccountMove(models.Model):
     )
     po = fields.Char(related="origin_id.po")
 
-    @api.depends("ref")
+    @api.depends("invoice_origin")
     def _get_sale_order_origin(self):
         for r in self:
-            r.origin_id = self.env["sale.order"].search([("name", "=", r.ref)], limit=1)
+            r.origin_id = self.env["sale.order"].search([("name", "=", r.invoice_origin)], limit=1)
